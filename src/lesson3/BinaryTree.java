7e9e619f5fe84df2ce74874c29212f82b51bb62f
mizunotlt <dimitry.arhireeff@gmail.com>
2017-11-12 19:07:56 +0300

    дз по графам

--- src/lesson3/BinaryTree.java
+++ src/lesson3/BinaryTree.java
@@ -19,11 +19,12 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         Node(T value) {
             this.value = value;
         }
+
     }
 
-    private Node<T> root = null;
+    private Node<T> root = null; //Корень дерева
 
-    private int size = 0;
+    private int size = 0; //Глубина дерева
 
     @Override
     public boolean add(T t) {
@@ -35,12 +36,10 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         Node<T> newNode = new Node<>(t);
         if (closest == null) {
             root = newNode;
-        }
-        else if (comparison < 0) {
+        } else if (comparison < 0) {
             assert closest.left == null;
             closest.left = newNode;
-        }
-        else {
+        } else {
             assert closest.right == null;
             closest.right = newNode;
         }
@@ -58,11 +57,91 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         Node<T> right = node.right;
         return right == null || right.value.compareTo(node.value) > 0 && checkInvariant(right);
     }
+    private Node<T> findParent(T value) {
+        if ((root == null) || (root.value == value)) return null;
+        return findParent(root, value);
+    }
+    private Node<T> findParent(Node<T> start, T value) {
+        //Если нашли родительский узел
+        if ((start.left != null && start.left.value == value) || (start.right != null && start.right.value == value))
+            return start;
+        int comparison = value.compareTo(start.value);
+        if (comparison == 0) {
+            return start;
+        } else if (comparison < 0) {
+            if (start.left == null) return start;
+            return findParent(start.left, value);
+        } else {
+            if (start.right == null) return start;
+            return findParent(start.right, value);
+        }
+    }
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
+        Node<T> node = find((T) o);
+        if (node == null)
+            return false;
+        //Нет дочерних узлов
+        if ((node.left == null) && (node.right == null)) {
+            if (findParent(root, node.value).right != null) {
+                if (findParent(root, node.value).right.value == (T) o) {
+                    findParent(root, node.value).right = null;
+                    return true;
+                }
+            }
+            if (findParent(root, node.value).left != null) {
+                if (findParent(root, node.value).left.value == (T) o) {
+                    findParent(root, node.value).left = null;
+                    return true;
+                }
+            }
+        }
+        //левый дочерний узел
+        if ((node.left != null) && (node.right == null)) {
+            if(findParent(root, node.value).left.value == node.value){
+                findParent(root, node.value).left = node.left;
+                return true;
+            }
+            if(findParent(root, node.value).right.value == node.value){
+                findParent(root, node.value).right = node.left;
+                return true;
+            }
+        }
+        //правый дочерний узел
+        if ((node.left == null) && (node.right != null)) {
+            Node <T> snode = findParent(node.value);
+            if(findParent(root, node.value).left.value == node.value){
+                findParent(root, node.value).left = node.right;
+                return true;
+            }
+            if(findParent(root, node.value).right.value == node.value){
+                findParent(root, node.value).right = node.right;
+                return true;
+            }
+        }
+        //зел имеет и левый дочерний узел и правый
+        if ((node.left != null) && (node.right != null)){
+            Node <T> tempLeft = findLeftNull(find((T) o).right);
+            tempLeft.left = find((T) o).left;
+            findParent((T) o).right = tempLeft;
+            return true;
+        }
+
+        return false;
     }
+    private Node<T> findLeftNull(Node<T> node) {
+        if (node == null) {
+            return null; //
+        }
+        if (node.left != null) {
+            return findLeftNull(node.left);
+        }
+        return node;
+    }
+
+
+
 
     @Override
     public boolean contains(Object o) {
@@ -99,11 +178,16 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         private BinaryTreeIterator() {}
 
         private Node<T> findNext() {
-            throw new UnsupportedOperationException();
+            if (current.right != null)
+                return findMin(current);
+            //дошли до самого нижнего правого
+            else if (current.value.compareTo(nodeToCompare(current).value) < 0) return nodeToCompare(current);
+            return null;
         }
 
         @Override
         public boolean hasNext() {
+
             return findNext() != null;
         }
 
@@ -118,6 +202,19 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         public void remove() {
             throw new UnsupportedOperationException();
         }
+    }
+    private Node<T> findMin(Node<T> current){
+        current = current.right;
+        while(current.left != null) current = current.left;
+        return current;
+    }
+    private Node<T> nodeToCompare(Node<T> node){
+        Node<T> current = root;
+
+        if (current.value.compareTo(node.value) > 0 && current.left.value != node.value) current = current.left;
+        else if (current.value.compareTo(node.value) < 0 && current.right.value != node.value) current = current.right;
+
+        return current;
     }
 
     @NotNull
@@ -175,4 +272,5 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         }
         return current.value;
     }
+
 }
--- test/lesson3/BinaryTreeTest.kt
+++ test/lesson3/BinaryTreeTest.kt
@@ -1,59 +1,35 @@
 package lesson3
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class BinaryTreeTest {
     @Test
-    fun add() {
+    fun Tets1() {
         val tree = BinaryTree<Int>()
         tree.add(10)
         tree.add(5)
         tree.add(7)
         tree.add(10)
-        assertEquals(3, tree.size)
-        assertTrue(tree.contains(5))
         tree.add(3)
         tree.add(1)
-        tree.add(3)
         tree.add(4)
-        assertEquals(6, tree.size)
-        assertFalse(tree.contains(8))
+        tree.add(6)
         tree.add(8)
-        tree.add(15)
-        tree.add(15)
-        tree.add(20)
-        assertEquals(9, tree.size)
-        assertTrue(tree.contains(8))
-        assertTrue(tree.checkInvariant())
-        assertEquals(1, tree.first())
-        assertEquals(20, tree.last())
+        tree.add(9)
+        assertTrue(tree.remove(7))
+        System.out.print(tree.toString());
     }
-
     @Test
-    fun addKotlin() {
-        val tree = KtBinaryTree<Int>()
+    fun Tets2() {
+        val tree = BinaryTree<Int>()
         tree.add(10)
         tree.add(5)
         tree.add(7)
-        tree.add(10)
-        assertEquals(3, tree.size)
-        assertTrue(tree.contains(5))
-        tree.add(3)
-        tree.add(1)
-        tree.add(3)
-        tree.add(4)
-        assertEquals(6, tree.size)
-        assertFalse(tree.contains(8))
-        tree.add(8)
-        tree.add(15)
-        tree.add(15)
-        tree.add(20)
-        assertEquals(9, tree.size)
-        assertTrue(tree.contains(8))
-        assertTrue(tree.checkInvariant())
-        assertEquals(1, tree.first())
-        assertEquals(20, tree.last())
+        assertTrue(tree.remove(5))
+        System.out.print(tree.toString());
     }
+
+
+
 }
